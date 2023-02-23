612f7584e72a43d616628dbc6ad8969a47b43686
Harpret890 <w0802461@myscc.ca>
2023-02-23 18:22:22 -0500

    pipe

--- h_kaur_ChocolateBrands/src/app/app.module.ts
+++ h_kaur_ChocolateBrands/src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
-
+import { ContentListPipe } from './content-list/content-list.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    ContentCardComponent,
-    ContentListComponent
+    ContentListComponent,
+    ContentListPipe
   ],
   imports: [
     BrowserModule
