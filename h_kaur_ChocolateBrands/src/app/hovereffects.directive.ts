4bdc25a58576346b1bc41a95844d4aeaba60bc82
Harpret890 <w0802461@myscc.ca>
2023-03-03 16:04:13 -0500

    updated the content list html with hovereffects and tags

--- h_kaur_ChocolateBrands/src/app/hovereffects.directive.ts
+++ h_kaur_ChocolateBrands/src/app/hovereffects.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
-
+import { Directive, ElementRef, HostListener } from '@angular/core';
 @Directive({
   selector: '[appHovereffects]'
 })
 export class HovereffectsDirective {
 
-  constructor() { }
+  
+  constructor(private elm: ElementRef) {}
+  @HostListener('mouseenter') onMouseEnter(){
+    this.elm.nativeElement.style.textDecoration = 'underline';
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this.elm.nativeElement.style.textDecoration = 'none';
+  }
 
 }
