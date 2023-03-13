d7d6233705a1291503dfdedfcdb348f306779fab
Harpret890 <w0802461@myscc.ca>
2023-03-13 18:45:46 -0400

    updated the service with an observerable

--- h_kaur_ChocolateBrands/src/app/chocolsteservices/content.service.ts
+++ h_kaur_ChocolateBrands/src/app/chocolsteservices/content.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { contentsList } from '../helper-files/contentDb';
+import { Content } from '../helper-files/content-interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  constructor() {}
+    getContent() : Observable<Content[]>{
+      return of(contentsList)
+      }
+  
 }
