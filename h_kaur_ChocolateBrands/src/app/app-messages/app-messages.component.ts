ff0f16a79959c3dbc8445feafaeaddd24787cc00
Harpret890 <w0802461@myscc.ca>
2023-03-17 13:53:26 -0400

    message works

--- h_kaur_ChocolateBrands/src/app/app-messages/app-messages.component.ts
+++ h_kaur_ChocolateBrands/src/app/app-messages/app-messages.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-app-messages',
   templateUrl: './app-messages.component.html',
   styleUrls: ['./app-messages.component.scss']
 })
 export class AppMessagesComponent {
-
+  constructor(public messageService: MessageService) {}
 }
