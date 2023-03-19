fe322f8fa9b9ea6c0febaf631321bac1731b0997
Harpret890 <w0802461@myscc.ca>
2023-03-19 17:13:24 -0400

    updated service servers

---
+++ h_kaur_ChocolateBrands/src/app/server.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from "angular-in-memoryweb-api";
+import { Content } from './helper-files/content-interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  constructor() { }
+}
+export class InMemoryDataService implements
+InMemoryDbService {
+createDb() {
+// setting it to the value of our array of content
+const content : Content[] = CONTENT;
+return {content};
+}
