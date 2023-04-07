7131c5abafef1138a6309c02140ee906830f3e08
Harpret890 <w0802461@myscc.ca>
2023-04-07 09:17:03 -0400

    added dialoug box for adding new content

--- h_kaur_ChocolateBrands/src/app/modify-content-component/modify-content-component.component.ts
+++ h_kaur_ChocolateBrands/src/app/modify-content-component/modify-content-component.component.ts
@@ -1,19 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import {MatButtonModule} from '@angular/material/button';
-
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 @Component({
   selector: 'app-modify-content-component',
   templateUrl: './modify-content-component.component.html',
   styleUrls: ['./modify-content-component.component.scss']
 })
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
 export class ModifyContentComponentComponent {
-  contentsList: any;
-  id: null | undefined;
-  addContentToList(newContentItem: Content): void {
-    this.contentsList.addContent(newContentItem)
-    .subscribe((newContentFromServer: any) =>
-    this.contentsList.push(newContentFromServer)
-    );
+  animal: string | undefined;
+  name: string | undefined;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: {name: this.name, animal: this.animal},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+}
+
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
