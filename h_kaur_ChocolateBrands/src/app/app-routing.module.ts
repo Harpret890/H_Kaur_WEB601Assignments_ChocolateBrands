4c00e9b58519a13722da44598194884cc6f29ca8
Harpret890 <w0802461@myscc.ca>
2023-04-13 17:00:10 -0400

    added routing connections for pagenotfound and message

--- h_kaur_ChocolateBrands/src/app/app-routing.module.ts
+++ h_kaur_ChocolateBrands/src/app/app-routing.module.ts
@@ -2,16 +2,42 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ContentListComponent } from './content-list/content-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DetailedContentComponent } from './detailed-content/detailed-content.component';
 
 const routes: Routes = [{
-  path: "content",
-  component: ContentListComponent,
-}
-];
+  path: ""
+,
+redirectTo: "/content"
+,
+pathMatch: "full"
+,
+},
+{ path: "content"
+, component: ContentListComponent },
+{ path: "content/:id"
+, component:
+DetailedContentComponent },
+{ path: "**"
+, component: PageNotFoundComponent }
 
+];
+ngOnInit(){
+  this.route.paramMap.subscribe(params => {
+    this.id = Number(params.get("id") ?? 0);
+    this.contentService.getContentItem(this.id)
+    .subscribe((c) => {
+    this.content = c;
+    });
+  });
+  }
 @NgModule({
   declarations: [],
   imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
--- h_kaur_ChocolateBrands/src/app/hero.service.ts
+++ h_kaur_ChocolateBrands/src/app/hero.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ContentService } from './chocolsteservices/content.service';
+import { Content } from './helper-files/content-interface';
 import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
-  }
+  // constructor(private messageService: MessageService) { }
+  // getHeroes(): Observable<Hero[]> {
+  //   const heroes = of(HEROES);
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return heroes;
+  // }
+  id: number | undefined;
+  content!: Content;
+constructor(private route: ActivatedRoute,
+private contentService: ContentService) {}
 }
