77346edd4a7dfd4c8a784cceefc929e02df72399
Harpret890 <w0802461@myscc.ca>
2023-04-13 16:27:56 -0400

    Connecting the contentlistcompnets with the routes

--- h_kaur_ChocolateBrands/src/app/app-routing.module.ts
+++ h_kaur_ChocolateBrands/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
 
-
+const routes: Routes = [{
+  path: "content",
+  component: ContentListComponent,
+}
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
